c40a92afdb4168636253d6b5660db68521f425c9
weian.song <weian.song@beyonca.com>
2023-04-01 00:41:18 +0800

    init project

---
+++ src/main/java/org/openprojectx/gateway/core/GatewayCoreApplication.java
@@ -0,0 +1,13 @@
+package org.openprojectx.gateway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayCoreApplication.class, args);
+	}
+
+}
